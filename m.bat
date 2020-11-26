70bea35789f29d64a6ffeaea7a46d2556e380dfb
ilikui <2563043733@qq.com>
2020-11-26 20:51:56 +0800

    1/26_0/_20_20_51

--- m.bat
+++ m.bat
@@ -5,14 +5,25 @@ rem  the path of the git command
 
 set git = D:\Git\bin\git.exe
 
+set d=%date:~0,2%
+set mo=%date:~3,2%
+set y=%date:~6,4%
+set h=%time:~0,2%
+set mi=%time:~3,2%
+
+
  git add *
  
 
  
- git commit -m "update"
+ git commit -m %y%_%mo%_%d%_%h%_%mi%
  
  git push
  
  git status
 
 pause
+
+@echo off
+
+@echo on
