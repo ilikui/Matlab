a43052fd04acf97ea5fc4dc2207fbb06df19a3f0
ilikui <2563043733@qq.com>
2020-11-26 20:53:46 +0800

    upinfo

--- m.bat
+++ m.bat
@@ -3,27 +3,28 @@ echo off
 rem  the path of the git command
 
 
-set git = D:\Git\bin\git.exe
+	set git = D:\Git\bin\git.exe
 
-set d=%date:~0,2%
-set mo=%date:~3,2%
-set y=%date:~6,4%
-set h=%time:~0,2%
-set mi=%time:~3,2%
+	set d=%date:~0,2%
+	set mo=%date:~3,2%
+	set y=%date:~6,4%
+	set h=%time:~0,2%
+	set mi=%time:~3,2%
+	
+	set upinfo = %y%_%mo%_%d%_%h%_%mi%
 
 
- git add *
- 
+	 git add *
+	 
 
- 
- git commit -m %y%_%mo%_%d%_%h%_%mi%
- 
- git push
- 
- git status
+	 
+	 git commit -m  "upinfo"
+	 
+	 git push
+	 
+	 git status
 
 pause
 
 @echo off
 
-@echo on
