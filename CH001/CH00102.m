055cc624dbff5f3163db24baf788f55ac2743cf3
ilikui <2563043733@qq.com>
2020-11-28 20:35:16 +0800

    update by likui

---
+++ CH001/CH00102.m
@@ -0,0 +1,9 @@
+%% Matlab里面的常量
+
+fprintf("pi的值是%d \n",pi)
+
+fprintf("0/0的值是%s \n",0/0)
+fprintf("1/0的值是%s \n",1/0)
+fprintf("eps的值是%ld \n",eps)
+fprintf("realmin的值是%ld \n",realmin)
+fprintf("realmax的值是%ld \n",realmax)
