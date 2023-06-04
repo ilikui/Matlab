468d21da64ed2465d8cb14a6074d68a2f49c438d
ilikui <2563043733@qq.com>
2023-06-04 18:54:36 +0800

    update by likui

--- Src/App/PyWebRead.m
+++ Src/App/PyWebRead.m
@@ -35,16 +35,56 @@ Mdata  =  jsondecode(string(Response.text));
 
     
 %     RequestHttp =BaseHttp+x;
-%     Response = py.requests.get(RequestHttp);
+
+%Response = py.requests.get(RequestHttp);
 % 
 % 
 %    
 % 
 %     
 %     
-%     [returnAudio,returnFs] = webread(Mdata.url,weboptions('ContentType','audio','Timeout',60));
-% 
-%     sound(returnAudio,returnFs)
+    [returnAudio,returnFs] = webread(Mdata.url,weboptions('ContentType','audio','Timeout',60));
+
+    % sound(returnAudio,returnFs)
+    
+    
+    %% settings
+div = 0.2;
+y = returnAudio;
+
+Fs = returnFs;
+%% read file
+
+T = 1/Fs;
+L = length(y)-mod(length(y),2);
+t = (0:L-1)*T;
+l = div*Fs;
+
+%% HPF
+Hlp = btw;
+y1 = filtfilt(Hlp.sosMatrix, 1, y);
+
+%% play music
+sound(y,Fs);
+
+%% FFT
+for i=1:L/(l-1)
+    t1 = clock;
+    x = y1(i*l:(i+1)*l);
+    FFT(x,l,Fs);
+    t2 = etime(clock,t1);
+    pause(div-t2);
+end
+
+function [] = FFT(x,l,Fs)
+Y = fft(x);
+P2 = abs(Y/l);
+P1 = P2(1:l/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+f = Fs*(0:(l/2))/l;
+plot(f,P1);
+%axis([1e3 1.8e4 0 0.15]);
+end
 
  
 
