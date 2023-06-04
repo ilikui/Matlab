ceb3ca878b9c64802f694a10a4e8179b70c69646
ilikui <2563043733@qq.com>
2023-06-04 20:09:25 +0800

    update by likui

---
+++ Src/Program/gcc/hello.c
@@ -0,0 +1,17 @@
+
+/**
+ * @brief  This is example C code
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+#include  <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a = 1;
+    int b = 12;
+
+    printf("a+b = ",a+b);
+    return 0;
+}
---
+++ Src/Program/mex/HelloWorld.c
@@ -0,0 +1,34 @@
+/*=================================================================
+ *
+ * YPRIME.C	Sample .MEX file corresponding to YPRIME.M
+ *	        Solves simple 3 body orbit problem 
+ *
+ * The calling syntax is:
+ *
+ *		[yp] = yprime(t, y)
+ *
+ *  You may also want to look at the corresponding M-code, yprime.m.
+ *
+ * This is a MEX-file for MATLAB.  
+ * Copyright 1984-2017 The MathWorks, Inc.
+ *
+ *=================================================================*/
+#include <math.h>
+#include "mex.h"
+
+
+
+void mexFunction( int nlhs, mxArray *plhs[], 
+		  int nrhs, const mxArray*prhs[] )     
+{ 
+    
+    int a,b,c;
+    
+    a =12;
+    c =a+13;
+    mexPrintf("c= %d\n",c);
+    return;
+}
+
+/* LocalWords:  yp maxlhs
+ */
